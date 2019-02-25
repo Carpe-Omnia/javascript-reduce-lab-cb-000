0ea0a0b4e210c7106b26cd85e91d96ef66218131
Tomaz Rodrigues <tomaz.r.rodrigues@gmail.com>
2019-02-25 18:17:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,11 +17,13 @@ const monologueLines = [
   'I am the one who knocks!'
 ];
 
-const totalBatteries = batteryBatches.reduce(batCount, 0)
+const totalBatteries = batteryBatches.reduce(batCount, 0) ;
 function batCount(accumulator, currentValue) {
   return accumulator + currentValue ;
 }
+
 const wordCountMap = monologueLines.reduce(mapper, {}) ;
+
 function mapper(accumulator, currentValue) {
   var words = `${currentValue.split(" ").size}` ;
   !!accumulator.words ? acumulator.words += 1 : accumulator.words = 1 ;
