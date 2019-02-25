269a8fe4c91951e238395220d7068aff17d9e56c
Tomaz Rodrigues <tomaz.r.rodrigues@gmail.com>
2019-02-25 18:15:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -21,9 +21,9 @@ const totalBatteries = batteryBatches.reduce(batCount, 0)
 function batCount(accumulator, currentValue) {
   return accumulator + currentValue ;
 }
-const wordCountMap = monologueLines.reduce(mapper, {}) ; 
+const wordCountMap = monologueLines.reduce(mapper, {}) ;
 function mapper(accumulator, currentValue) {
-  var words = currentValue.split(" ").size ; 
-  !!accumulator.words ? acumulator.words += 1 : accumulator.words = 1 ; 
-   
+  var words = currentValue.split(" ").size ;
+  !!accumulator.words ? acumulator.words += 1 : accumulator.words = 1 ;
+
 }
